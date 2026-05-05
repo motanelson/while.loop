d607fcff7e9f0b0ea6f4bda616dc6cace6e0ad35
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-05-05 19:18:45 +0100

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,10 @@
+password="  "
+print("give me the password and enter")
+while 1:
+   a=input()
+   if a==password:
+       print("correct code\n")
+       break
+   else:
+       print("password error:\n")
+
